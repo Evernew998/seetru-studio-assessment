3319d5d3945953ed6089d548adf644ea26408f97
Salahuddin <salahuddinhamzah998@gmail.com>
2024-02-23 00:56:49 +0800

    configuring header component

--- ISFBWebApp/src/App.jsx
+++ ISFBWebApp/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './components/css/App.css'
+import './components/css/Header.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="header">
+      <div className='header-social-media'></div>
+      <div className='header-right-side'>
+        <nav className='header-nav'>
+          <a href=''>E-Learning</a>
+          <a href=''>FAQ</a>
+          <a href=''>Career</a>
+          <a href=''>Contact Us</a>
+        </nav>
+        <div className='language-toggle'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
   )
 }
 
