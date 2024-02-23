c5f5e27a47b0acbdf4313e396b8bd459f5bf1d7d
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-23 18:17:39 +0800

    added navigation bar

--- ISFBWebApp/src/App.jsx
+++ ISFBWebApp/src/App.jsx
@@ -1,19 +1,58 @@
 import './components/css/App.css'
 import './components/css/Header.css'
+import './components/css/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-left-content'>
+
+      </div>
+      <div className='navbar-right-content'>
+        <div className='main-nav'>
+          <a href='' style={{marginRight: '32px'}}>Home</a>
+          <a href='' style={{marginRight: '41px'}}>About IFSB</a>
+          <a href='' style={{marginRight: '34px'}}>Standard Development</a>
+          <a href='' style={{marginRight: '34px'}}>Publication</a>
+          <a href='' style={{marginRight: '42px'}}>PSIFIs</a>
+          <a href='' style={{marginRight: '30px'}}>Consultation</a>
+          <a href='' style={{marginRight: '35px'}}>Implementation</a>
+          <a href=''>Event & Activities</a>
+        </div>
+        <div className='navbar-member-zone'>
+          <img src='' alt='pro'/>
+          <a href=''>{"IFSB Member's Zone"}</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className='header-social-media'></div>
-      <div className='header-right-side'>
-        <nav className='header-nav'>
-          <a href=''>E-Learning</a>
-          <a href=''>FAQ</a>
-          <a href=''>Career</a>
-          <a href=''>Contact Us</a>
-        </nav>
-        <div className='language-toggle'></div>
+    <div className='header-container'>
+      <div className="header">
+        <div className='header-left-content'>
+          <div className='header-social-media'>
+            <a href=''></a>
+          </div>
+        </div>
+        <div className='header-right-content'>
+          <nav className='header-nav'>
+            <a href='' style={{marginRight: '46px'}}>E-learning</a>
+            <a href='' style={{marginRight: '50px'}}>FAQ</a>
+            <a href='' style={{marginRight: '45px'}}>Career</a>
+            <a href=''>Contact Us</a>
+          </nav>
+          <div className='header-language-toggle'>
+            <button>EN</button>
+            <div className='language-toggle-line'></div>
+            <button>AR</button>
+          </div>
+        </div>
       </div>
+      <div className='header-horizontal-bar'></div>
+      <Navbar />
     </div>
   )
 }
@@ -21,6 +60,7 @@ const App = () => {
   return (
     <div>
       <Header />
+      <div style={{height: '1920px'}}></div>
     </div>
   )
 }
