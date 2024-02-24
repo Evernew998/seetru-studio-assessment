c7e5fbf2a5e8566b3f18cdd8aad6583de8f5afce
Salahuddin <salahuddinhamzah998@gmail.com>
2024-02-25 00:21:53 +0800

    newsletter finishes

--- ISFBWebApp/src/components/Newsletter.jsx
+++ ISFBWebApp/src/components/Newsletter.jsx
@@ -1,14 +1,21 @@
 import './css/Newsletter.css'
+import upArrow from '../assets/up-arrow-icon.svg'
 
 const Newsletter = () => {
   return (
     <div className='newsletter'>
+      <button 
+        className='up-button' style={{backgroundImage: `url(${upArrow})`}}
+        onClick={() => window.scrollTo(0, 0)}
+      >
+      </button>
       <div className='newsletter-text'>
         <h4>Newsletter</h4>
         <p>Subscribe To Our News Letter And Get Our Latest Updates</p>
       </div>
       <form className='newsletter-form'>
         <div className='newsletter-input-wrapper'>
+          <div></div>
           <input type='text' name='subscribe-email' placeholder='Your Email' />
           <button>Subscribe Now</button>
         </div>
